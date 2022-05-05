b94e7ca7009cc5c9d13401c9b21305a573f89dbb
Goloshchuk Svetlana <svetagoloshuk@gmail.com>
2022-05-05 15:14:35 +0300

    add script fieGhdb

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
-const arr = [1,2,3];
+const arr = [1, 2, 3];
 
 console.log(arr[1]);
 
 
 //const obj = {a: 1, b: 2};
 const obj = {
-    Anna: 500
-    Alice: 600
+    'Anna': 500,
+    'Alice': 600
 };
-console.log(Alice);
+console.log(obj.Anna);
+
+
+const arrObj = { //новое свойство внутри объекта
+    a: 'a',
+    1: 'b',
+    2: 'c',
+    abc: {
+        df: [{
+            as: 'q'
+        }, {
+            dfd: 'hj'
+        }],
+        def: {
+            d: "hule"
+
+        }
+    }
+};
+const b = 'b';
+const d ='fref';
+arrObj.b = '1235';
+console.log(arrObj[b]);
+console.log(arrObj.b);
+console.log(arrObj.abc.df[0].as);
+
+console.log(5 % 2); //остаток 1 - деление с остатком
+console.log(2 * 5 == 9); //false
+
+const isChecked = false,
+    isClose = true;
+console.log(isChecked || !isClose);
+
