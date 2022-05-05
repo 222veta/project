9cfd8159cc68fc8895f379e22f6ba5a4c6e3935a
222veta <svetagoloshuk@gmail.com>
2022-05-05 19:23:47 +0300

    game_start_aboutFilm

---
+++ js/script2.js
@@ -0,0 +1,12 @@
+console.log('att'+ "-onu"); //att-onu
+console.log(4 +"5"); // 45
+console.log(4 + +"5");  // 
+
+
+let incr = 10,
+ decr = 10;
+
+ incr++;
+ decr--;
+ console.log(incr);
+ console.log(decr);
